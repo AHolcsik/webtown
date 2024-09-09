f6beb1bf6d6f053b77ea44999cfed55bc092a25e
A Holcsik <you@example.com>
2024-09-09 23:23:50 +0200

    added language selector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+/* eslint-disable no-unused-expressions */
 import Masonry from "./components/Masonry";
+import Timeline from "./components/Timeline";
 
 function App() {
+  const setRTL = () => {
+    document.querySelector("html").setAttribute("dir", "rtl");
+    document.querySelector("html").setAttribute("lang", "ar");
+  };
+
+  const setLTR = () => {
+    document.querySelector("html").setAttribute("dir", "ltr");
+    document.querySelector("html").setAttribute("lang", "eng");
+  };
+
+  const chageDirection = () => {
+    if (document.querySelector("html").getAttribute("dir") === "rtl") setLTR();
+    else setRTL();
+  };
+
   return (
-    <div className="container">
-      <Masonry />
-    </div>
+    <>
+      <button onClick={chageDirection}>Change language</button>
+      <div>
+        <Timeline />
+      </div>
+      <div className="container">
+        <Masonry />
+      </div>
+    </>
   );
 }
 
